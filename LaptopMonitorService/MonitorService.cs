9c669d71395126853f2108f1e6eaa526a7525fa8
Frashier <75584735+Frashier@users.noreply.github.com>
2022-01-16 15:42:42 +0100

    feat: implemented WCF in the service

--- LaptopMonitorService/MonitorService.cs
+++ LaptopMonitorService/MonitorService.cs
@@ -1,52 +1,81 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Diagnostics;
-using System.Linq;
+﻿using System.Diagnostics;
 using System.ServiceProcess;
-using System.Text;
 using System.Threading;
+using System.Configuration;
+using WCFMonitorLibrary;
+using System.ServiceModel;
+using System.ServiceModel.Description;
+using System;
 
 namespace LaptopMonitorService
 {
     public partial class MonitorService : ServiceBase
     {
-        private const string EventLogName = "LaptopMonitorLog";
-        private const string SourceName = "LaptopMonitorSource";
         private readonly EventLog LaptopMonitorLog;
+        private readonly ServiceHost Host;
 
         public MonitorService()
         {
-            LaptopMonitorLog = SetupEventLog();
+            LaptopMonitorLog = SetupEventLog(ConfigurationManager.AppSettings["EventLogName"], ConfigurationManager.AppSettings["SourceName"]);
+            Host = SetupHost(new Uri(ConfigurationManager.AppSettings["BaseAddress"]));
+
             InitializeComponent();
+        }
+
+        private ServiceHost SetupHost(Uri baseAddress)
+        {
+            ServiceHost host = new ServiceHost(typeof(WCFService), baseAddress);
+            host.AddServiceEndpoint(typeof(IWCFService), new WSHttpBinding(), "WCFService");
+
+            ServiceMetadataBehavior smb = new ServiceMetadataBehavior();
+            smb.HttpGetEnabled = true;
+            host.Description.Behaviors.Add(smb);
+
+            return host;
         }
 
         /// <summary>
         /// Method used for setting up event log for the service
         /// </summary>
         /// <returns></returns>
-        private EventLog SetupEventLog()
+        private EventLog SetupEventLog(string eventLogName, string sourceName)
         {
-            EventLog newEventLog = new EventLog(EventLogName);
+            EventLog newEventLog = new EventLog(eventLogName);
 
             // Create source if it doesn't exist
-            if (!EventLog.SourceExists(SourceName, "."))
+            if (!EventLog.SourceExists(sourceName, "."))
             {
-                EventLog.CreateEventSource(SourceName, EventLogName);
+                EventLog.CreateEventSource(sourceName, eventLogName);
                 Thread.Sleep(1000);
             }
 
-            newEventLog.Source = SourceName;
+            newEventLog.Source = sourceName;
             return newEventLog;
         }
 
         protected override void OnStart(string[] args)
         {
+            try
+            {
+                Host.Open();
+            } 
+            catch (Exception)
+            {
+                Host.Abort();
+            }
+            
         }
 
         protected override void OnStop()
         {
+            try
+            {
+                Host.Close();
+            }
+            catch (Exception)
+            {
+                Host.Abort();
+            }
         }
     }
 }
