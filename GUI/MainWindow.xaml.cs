d1f1fcb784cdae37aa3f32c5869c9cdba2ff72c7
Frashier <75584735+Frashier@users.noreply.github.com>
2022-01-17 20:23:40 +0100

    feat: changed GUI to net framework 4.7.2 and implemented wcf client

---
+++ GUI/MainWindow.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows;
+using GUI.ServiceReference1;
+
+namespace GUI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        WCFServiceClient Client;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            Client = new WCFServiceClient();
+            ConsoleTextBlock.Text = Client.Echo("Test");
+        }
+    }
+}
